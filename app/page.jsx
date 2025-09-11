f6639aec58a09d13134ec7eacee979989346892c
andresengler <141840530+andresengler@users.noreply.github.com>
2025-09-11 14:24:28 -0300

    Delete app/page.jsx

--- app/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <main style={{ padding: 32, fontFamily: 'system-ui' }}>
-      <h1>Hola, Editado (Next 14)</h1>
-      <p>Deploy OK en Vercel con Next.js.</p>
-    </main>
-  );
-}
