95da64ac5e5e28a564a1c070e6681b38b591e27b
andresengler <141840530+andresengler@users.noreply.github.com>
2025-09-11 14:24:11 -0300

    Delete app/layout.jsx

--- app/layout.jsx
+++
@@ -1,9 +0,0 @@
-export const metadata = { title: 'Editado' };
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="es">
-      <body style={{ margin: 0 }}>{children}</body>
-    </html>
-  );
-}
